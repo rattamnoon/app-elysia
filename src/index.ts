d316d9a9f1bd95cef63a4ea4ca543b3aeb9fa0ca
UN <59855096+rattamnoon@users.noreply.github.com>
2023-12-31 00:22:41 +0700

    install graphql
    Signed-off-by: UN <59855096+rattamnoon@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,75 @@
+import { apollo, gql } from "@elysiajs/apollo";
+import bearer from "@elysiajs/bearer";
+import { cron } from "@elysiajs/cron";
+import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia();
+
+app
+  .use(bearer())
+  .use(
+    cron({
+      name: "heartbeat",
+      pattern: "*/10 * * * * *",
+      run() {
+        console.log("Heartbeat");
+      },
+    })
+  )
+  .get("/", ({ bearer }) => bearer, {
+    beforeHandle({ bearer, set }) {
+      if (bearer !== "Bearer xxxxx") {
+        set.status = 400;
+        set.headers[
+          "WWW-Authenticate"
+        ] = `Bearer realm='sign', error="invalid_request"`;
+
+        return "Unauthorized";
+      }
+
+      return "Hello Elysia";
+    },
+  })
+  .use(
+    swagger({
+      path: "/docs",
+    })
+  )
+  .use(
+    apollo({
+      typeDefs: gql`
+        type Book {
+          title: String
+          author: String
+        }
+
+        type Query {
+          books: [Book]
+        }
+      `,
+      resolvers: {
+        Query: {
+          books: () => {
+            return [
+              {
+                title: "Elysia",
+                author: "saltyAom",
+              },
+            ];
+          },
+        },
+      },
+      context: async ({ request }) => {
+        const authorization = request.headers.get("Authorization");
+
+        return {
+          authorization,
+        };
+      },
+    })
+  )
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
